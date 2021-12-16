dc0d4a97258ba96d562d308b318e6d9328547da1
Sam Tregea <sdtregea@gmail.com>
2021-12-15 22:02:34 -0500

    Create the spotify developer credentials

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+import os
+import json
+
+CLIENT_INFORMATION = json.loads(open(os.path.abspath('data/json/spotify_credentials.json'), 'r').read())
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return CLIENT_INFORMATION
+
+
+@app.route('/login')
+def login():
+    return "login here"
 
 
 if __name__ == '__main__':
